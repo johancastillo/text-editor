84e1de399b498e2cbb383e79c615382897db40f7
Johan Castillo <jcjohan2707@gmail.com>
2021-02-08 12:36:41 -0400

    SAVE code HTML in Database

--- save.php
+++ save.php
@@ -3,13 +3,14 @@
 <?php
 
 try{
+    $contenido = $_REQUEST["content"];
     // Conexión a la base de datos
     $connect = new PDO('mysql:host=localhost;dbname=text_editor', 'jcjohan', 'password');
     $connect ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
     // Guardar en la base de datos
     $statement = $connect->prepare("INSERT INTO posts (content) VALUES (:content)");
-    $statement->execute(["content" => "<p><b>Test Tree</b></p>"]);    
+    $statement->execute(["content" => $contenido]);    
     
     return "<h1>Saved</h1>";
     
